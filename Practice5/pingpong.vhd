c502eda50661d7c4b5777ca14406c063aa817265
MDGMDG <madago477@gmail.com>
2025-07-08 16:08:47 +0800

    fix: separate pointCount process

--- Practice5/pingpong.vhd
+++ Practice5/pingpong.vhd
@@ -2,30 +2,46 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity pingpong is
+entity pingpong_speed is
     Port (
         i_clk   : in  STD_LOGIC;
         i_rst   : in  STD_LOGIC;
         i_btn1  : in  STD_LOGIC;
         i_btn2  : in  STD_LOGIC;
-        o_LED   : out STD_LOGIC_VECTOR(7 downto 0)
+        o_led   : out STD_LOGIC_VECTOR(7 downto 0)
     );
-end pingpong;
+end pingpong_speed;
 
-architecture Behavioral of pingpong is
+architecture Behavioral of pingpong_speed is
+
     type STATE_TYPE is (IDLE, RIGHT_SHIFT, LEFT_SHIFT, FAIL);
 
     signal STATE     : STATE_TYPE := IDLE;
-    signal cntLED    : STD_LOGIC_VECTOR(9 downto 0) := (others => '0');
+
+    signal shift_reg   : STD_LOGIC_VECTOR(9 downto 0) := "0100000000";  
+    signal counter   : STD_LOGIC_VECTOR(25 downto 0) := (others => '0');
+    signal lfsr      : STD_LOGIC_VECTOR(7 downto 0) := "00000001";
     signal cntTime   : STD_LOGIC_VECTOR(3 downto 0) := (others => '0');
     signal cntPoint1 : STD_LOGIC_VECTOR(3 downto 0) := (others => '0');
     signal cntPoint2 : STD_LOGIC_VECTOR(3 downto 0) := (others => '0');
-    signal failSig   : STD_LOGIC := '0';
-    signal slowClk   : STD_LOGIC := '0';
-    signal counter   : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
+
+    signal slowClk    : STD_LOGIC := '0';   
+
+
 begin
 
-    --Process FSM
+    --process clockDivider
+    process(i_clk, i_rst)
+    begin
+        if i_rst = '0' then
+            counter <= (others => '0');
+        elsif rising_edge(i_clk) then
+            counter <= counter + 1;
+            slowClk <= counter(23);
+        end if;
+    end process;
+
+    --process FSM
     process(i_clk, i_rst)
     begin
         if i_rst = '0' then
@@ -33,118 +49,122 @@ begin
         elsif rising_edge(i_clk) then
             case STATE is
                 when IDLE =>
-                    if (i_btn1 = '1' AND cntLED = "0100000000") then
+                    if (i_btn1 = '1' and shift_reg = "0100000000") then
                         STATE <= RIGHT_SHIFT;
-                    else
-                        STATE <= IDLE;
                     end if;
                 when RIGHT_SHIFT =>
-                    if cntLED(1) = '1' and i_btn2 = '1' then
+                    if shift_reg = "0000000001" then
+                        STATE <= FAIL;
+                    elsif (shift_reg = "0000000010" and i_btn2 = '1') then
                         STATE <= LEFT_SHIFT;
-                    elsif failSig = '1' then
+                    elsif (i_btn2 = '1' and shift_reg /= "0000000010") then
                         STATE <= FAIL;
                     end if;
                 when LEFT_SHIFT =>
-                    if cntLED(8) = '1' and i_btn1 = '1' then
+                    if shift_reg = "1000000000" then
+                        STATE <= FAIL;
+                    elsif (shift_reg = "0100000000" and i_btn1 = '1') then
                         STATE <= RIGHT_SHIFT;
-                    elsif failSig = '1' then
+                    elsif (i_btn1 = '1' and shift_reg /= "0100000000") then
                         STATE <= FAIL;
                     end if;
                 when FAIL =>
-                    if cntTime = "1000" then
+                    if cntTime = "0111" then
                         STATE <= IDLE;
-                    else
-                        STATE <= FAIL;
                     end if;
             end case;
         end if;
     end process;
 
-    --Process clockDivider
-    process(i_clk, i_rst)
-    begin
-        if i_rst = '0' then
-            counter <= (others => '0');
-        elsif rising_edge(i_clk) then
-            counter <= counter + 1;
-        end if;
-    end process;
-
-    slowClk <= counter(23);   
-    --slowClk <= counter(4);  -- for simulation 
-
-    --Process counterLED 
+    --process LED_SHIFT
     process(slowClk, i_rst)
     begin
         if i_rst = '0' then
-            cntLED <= (others => '0');
+            shift_reg <= "0100000000";
         elsif rising_edge(slowClk) then
             case STATE is
                 when IDLE =>
-                    cntLED <= "0100000000";
+                    shift_reg <= "0100000000";
                 when RIGHT_SHIFT =>
-                    cntLED <= '0' & cntLED(9 downto 1);  -- 手動右移
+                    shift_reg <= '0' & shift_reg(9 downto 1);
                 when LEFT_SHIFT =>
-                    cntLED <= cntLED(8 downto 0) & '0';  -- 手動左移
-                when FAIL =>
-                    cntLED(8 downto 5) <= cntPoint1;
-                    cntLED(4 downto 1) <= cntPoint2;
+                    shift_reg <= shift_reg(8 downto 0) & '0';
                 when others =>
-                    null;
+                    null; 
             end case;
         end if;
     end process;
 
-    --Process counterPoint 
+    --process LED_output
     process(i_clk, i_rst)
     begin
         if i_rst = '0' then
-            cntPoint1 <= (others => '0');
-            cntPoint2 <= (others => '0');
+            o_led <= "10000000";
         elsif rising_edge(i_clk) then
-            if STATE = RIGHT_SHIFT and failSig = '1' then
-                cntPoint1 <= cntPoint1 + 1;
-            elsif STATE = LEFT_SHIFT and failSig = '1' then
-                cntPoint2 <= cntPoint2 + 1;
-            end if;
+            case STATE is
+                when IDLE =>
+                    o_led <= "10000000";
+                when RIGHT_SHIFT =>
+                    o_led <= shift_reg(8 downto 1);
+                when LEFT_SHIFT =>
+                    o_led <= shift_reg(8 downto 1);
+                when FAIL =>
+                    o_led(7 downto 4) <= cntPoint1;
+                    o_led(3 downto 0) <= cntPoint2;
+            end case;
         end if;
     end process;
 
-    --Process timer
+    --process timer
     process(slowClk, i_rst)
     begin
         if i_rst = '0' then
             cntTime <= (others => '0');
         elsif rising_edge(slowClk) then
-            if STATE = IDLE or STATE = RIGHT_SHIFT or STATE = LEFT_SHIFT then
-                cntTime <= (others => '0');
-            elsif STATE = FAIL then
+            if STATE = FAIL then
                 cntTime <= cntTime + 1;
+            else
+                cntTime <= (others => '0');
             end if;
         end if;
     end process;
 
-    --Process failDetect
+    --process pointCountLeft
     process(i_clk, i_rst)
     begin
         if i_rst = '0' then
-            failSig <= '0';
+            cntPoint1 <= (others => '0');
         elsif rising_edge(i_clk) then
-            if STATE = FAIL then
-                failSig <= '0';
-            elsif STATE = RIGHT_SHIFT then
-                if (cntLED(1) /= '1' and i_btn2 = '1') or cntLED = "0000000001" then
-                    failSig <= '1';
-                end if;
-            elsif STATE = LEFT_SHIFT then
-                if (cntLED(8) /= '1' and i_btn1 = '1') or cntLED = "1000000000" then
-                    failSig <= '1';
-                end if;
-            end if;
+            case STATE is
+                when RIGHT_SHIFT =>
+                    if shift_reg = "0000000001" then
+                        cntPoint1 <= cntPoint1 + 1;
+                    elsif (i_btn2 = '1' and shift_reg /= "0000000010") then
+                        cntPoint1 <= cntPoint1 + 1;
+                    end if;
+                when others =>
+                    null;   
+            end case;
         end if;
     end process;
 
-    -- LED Output
-    o_LED <= cntLED(8 downto 1);
+    --process pointCountRight
+    process(i_clk, i_rst)
+    begin
+        if i_rst = '0' then
+            cntPoint2 <= (others => '0');
+        elsif rising_edge(i_clk) then
+            case STATE is
+                when LEFT_SHIFT =>
+                    if shift_reg = "1000000000" then
+                        cntPoint2 <= cntPoint2 + 1;
+                    elsif (i_btn1 = '1' and shift_reg /= "0100000000") then
+                        cntPoint2 <= cntPoint2 + 1;
+                    end if;
+                when others =>
+                    null;   
+            end case;
+        end if;
+    end process;
 
 end Behavioral;
--- Practice6/drift_ball.vhd
+++ Practice6/drift_ball.vhd
@@ -15,21 +15,18 @@ end pingpong_speed;
 architecture Behavioral of pingpong_speed is
 
     type STATE_TYPE is (IDLE, RIGHT_SHIFT, LEFT_SHIFT, FAIL);
-    type SPEED_TYPE is (SLOW, MEDIUM, FAST);
 
     signal STATE     : STATE_TYPE := IDLE;
-    signal SPEED     : SPEED_TYPE := SLOW;
 
-    signal led_reg   : STD_LOGIC_VECTOR(9 downto 0) := "0100000000";  
+    signal shift_reg   : STD_LOGIC_VECTOR(9 downto 0) := "0100000000";  
     signal counter   : STD_LOGIC_VECTOR(25 downto 0) := (others => '0');
     signal lfsr      : STD_LOGIC_VECTOR(7 downto 0) := "00000001";
     signal cntTime   : STD_LOGIC_VECTOR(3 downto 0) := (others => '0');
     signal cntPoint1 : STD_LOGIC_VECTOR(3 downto 0) := (others => '0');
     signal cntPoint2 : STD_LOGIC_VECTOR(3 downto 0) := (others => '0');
-    signal failSig   : STD_LOGIC := '0';
 
     signal ledClk    : STD_LOGIC := '0';    -- LEDpattern觸發訊號
-    signal cntClk    : STD_LOGIC := '0';    -- FSM_SPEED、LFSR_random、timer觸發訊號
+    signal cntClk    : STD_LOGIC := '0';    -- LFSR_random、timer觸發訊號
 
 begin
 
@@ -40,15 +37,16 @@ begin
             counter <= (others => '0');
         elsif rising_edge(i_clk) then
             counter <= counter + 1;
-            case SPEED is
-                when FAST   => ledClk <= counter(21);  -- 最快
-                when MEDIUM => ledClk <= counter(23);  -- 中速
-                when SLOW   => ledClk <= counter(25);  -- 最慢
-            end case;
+            if lfsr(1 downto 0) = "00" then
+                ledClk <= counter(22);  -- SPEED_FAST 快速
+            elsif lfsr(1 downto 0) = "01" then
+                ledClk <= counter(23);  -- SPEED_MEDIUM 中速
+            elsif lfsr(1 downto 0) = "10" then
+                ledClk <= counter(24);  -- SPEED_SLOW 慢速
+            end if;
+            cntClk <= counter(24);
         end if;
     end process;
-
-    cntClk <= counter(25); 
 
 	--process LFSR_random
 	process(cntClk, i_rst)
@@ -60,25 +58,7 @@ begin
 		end if;
 	end process;
 
-	--process FSM_SPEED
-	process(cntClk, i_rst)
-	begin
-		if i_rst = '0' then
-			SPEED <= SLOW;
-		elsif rising_edge(cntClk) then
-			case lfsr(2 downto 0) is
-				when "000" | "001" =>
-					SPEED <= SLOW;
-				when "010" | "011" | "100" =>
-					SPEED <= MEDIUM;
-				when others =>
-					SPEED <= FAST;
-			end case;
-		end if;
-	end process;
-
-
-    --process FSM_PINGPONG
+    --process FSM
     process(i_clk, i_rst)
     begin
         if i_rst = '0' then
@@ -86,23 +66,23 @@ begin
         elsif rising_edge(i_clk) then
             case STATE is
                 when IDLE =>
-                    if (i_btn1 = '1' and led_reg = "0100000000") then
+                    if (i_btn1 = '1' and shift_reg = "0100000000") then
                         STATE <= RIGHT_SHIFT;
                     end if;
                 when RIGHT_SHIFT =>
-                    if led_reg = "0000000001" then
+                    if shift_reg = "0000000001" then
                         STATE <= FAIL;
-                    elsif (led_reg = "0000000010" and i_btn2 = '1') then
+                    elsif (shift_reg = "0000000010" and i_btn2 = '1') then
                         STATE <= LEFT_SHIFT;
-                    elsif (i_btn2 = '1' and led_reg /= "0000000010") then
+                    elsif (i_btn2 = '1' and shift_reg /= "0000000010") then
                         STATE <= FAIL;
                     end if;
                 when LEFT_SHIFT =>
-                    if led_reg = "1000000000" then
+                    if shift_reg = "1000000000" then
                         STATE <= FAIL;
-                    elsif (led_reg = "0100000000" and i_btn1 = '1') then
+                    elsif (shift_reg = "0100000000" and i_btn1 = '1') then
                         STATE <= RIGHT_SHIFT;
-                    elsif (i_btn1 = '1' and led_reg /= "0100000000") then
+                    elsif (i_btn1 = '1' and shift_reg /= "0100000000") then
                         STATE <= FAIL;
                     end if;
                 when FAIL =>
@@ -113,22 +93,41 @@ begin
         end if;
     end process;
 
-    --process LEDpattern
+    --process LED_SHIFT
     process(ledClk, i_rst)
     begin
         if i_rst = '0' then
-            led_reg <= "0100000000";
+            shift_reg <= "0100000000";
         elsif rising_edge(ledClk) then
             case STATE is
                 when IDLE =>
-                    led_reg <= "0100000000";
+                    shift_reg <= "0100000000";
                 when RIGHT_SHIFT =>
-                    led_reg <= '0' & led_reg(9 downto 1);
+                    shift_reg <= '0' & shift_reg(9 downto 1);
                 when LEFT_SHIFT =>
-                    led_reg <= led_reg(8 downto 0) & '0';
+                    shift_reg <= shift_reg(8 downto 0) & '0';
+                when others =>
+                    null; 
+            end case;
+        end if;
+    end process;
+
+    --process LED_output
+    process(i_clk, i_rst)
+    begin
+        if i_rst = '0' then
+            o_led <= "10000000";
+        elsif rising_edge(i_clk) then
+            case STATE is
+                when IDLE =>
+                    o_led <= "10000000";
+                when RIGHT_SHIFT =>
+                    o_led <= shift_reg(8 downto 1);
+                when LEFT_SHIFT =>
+                    o_led <= shift_reg(8 downto 1);
                 when FAIL =>
-                    led_reg(8 downto 5) <= cntPoint1;
-                    led_reg(4 downto 1) <= cntPoint2;
+                    o_led(7 downto 4) <= cntPoint1;
+                    o_led(3 downto 0) <= cntPoint2;
             end case;
         end if;
     end process;
@@ -147,25 +146,18 @@ begin
         end if;
     end process;
 
-    --process pointCount
+    --process pointCountLeft
     process(i_clk, i_rst)
     begin
         if i_rst = '0' then
             cntPoint1 <= (others => '0');
-            cntPoint2 <= (others => '0');
         elsif rising_edge(i_clk) then
             case STATE is
                 when RIGHT_SHIFT =>
-                    if led_reg = "0000000001" then
+                    if shift_reg = "0000000001" then
                         cntPoint1 <= cntPoint1 + 1;
-                    elsif (i_btn2 = '1' and led_reg /= "0000000010") then
+                    elsif (i_btn2 = '1' and shift_reg /= "0000000010") then
                         cntPoint1 <= cntPoint1 + 1;
-                    end if;
-                when LEFT_SHIFT =>
-                    if led_reg = "1000000000" then
-                        cntPoint2 <= cntPoint2 + 1;
-                    elsif (i_btn1 = '1' and led_reg /= "0100000000") then
-                        cntPoint2 <= cntPoint2 + 1;
                     end if;
                 when others =>
                     null;   
@@ -173,6 +165,23 @@ begin
         end if;
     end process;
 
-    o_led <= led_reg(8 downto 1);
+    --process pointCountRight
+    process(i_clk, i_rst)
+    begin
+        if i_rst = '0' then
+            cntPoint2 <= (others => '0');
+        elsif rising_edge(i_clk) then
+            case STATE is
+                when LEFT_SHIFT =>
+                    if shift_reg = "1000000000" then
+                        cntPoint2 <= cntPoint2 + 1;
+                    elsif (i_btn1 = '1' and shift_reg /= "0100000000") then
+                        cntPoint2 <= cntPoint2 + 1;
+                    end if;
+                when others =>
+                    null;   
+            end case;
+        end if;
+    end process;
 
 end Behavioral;
